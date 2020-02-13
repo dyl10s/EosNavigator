c1b819e092742a200ec94817818bcaa145c96575
Dylan Strohschein <dylanstrohschein@gmail.com>
2020-02-13 03:04:37 -0500

    Update EosNav.php

--- Dist/EosNav.php
+++ Dist/EosNav.php
@@ -1,5 +1,3 @@
 ﻿<?php
-// outputs the username that owns the running php/httpd process
-// (on a system with the "whoami" executable in the path)
 echo(shell_exec('./DirectoryScanner ' . escapeshellcmd($_GET["path"])));
 ?>
